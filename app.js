009c6e9e70fbc469e454c91834c8c5b5b1defbbd
Diego Prats <diego.prats@gmail.com>
2015-07-15 23:37:27 -0700

    created single pagge app

--- app.js
+++ app.js
@@ -1,2 +1,35 @@
+// MODULE
+var myApp = angular.module('myApp', ['ngRoute', 'ngResource']);
 
-var weatherApp = angular.module('weatherApp',['ngRoute', 'ngResource']);
+// ROUTES
+myApp.config(function($routeProvider){
+
+	$routeProvider
+
+	.when('/', {
+		templateUrl: '/pages/main.html',
+		controller: 'mainController'
+
+	})
+	.when('/forecast', {
+		templateUrl: 'pages/forecast.html',
+		controller: 'forecastController'
+	})
+
+
+});
+
+
+// CONTROLLERS
+
+weatherApp.controller('mainController', ['$scope', function($scope){
+
+	console.log('Hello from mainController');
+
+}]);
+
+weatherApp.controller('forecastController', ['$scope', function($scope){
+
+	console.log('Hello from forecastController');
+
+}]);
--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
-var PORT = 3000;
+var PORT = 8000;
 
 app.get('/', function(req,res){
 
@@ -12,10 +12,29 @@ app.get('/', function(req,res){
 
 });
 
-app.get('/app.js', function(req,res){
+app.get('/index.html', function(req,res){
 
-	res.sendFile(path.join(__dirname + '/app.js'));
+	res.sendFile(path.join(__dirname + '/index.html'));
+
 });
+
+
+app.get('/app.js', function(req,res){
+	console.log('called app.js');
+	res.sendFile(path.join(__dirname + '/app.js'));
+
+});
+
+app.get('/pages/main.html', function(req,res){
+	console.log('main requested');
+	res.sendFile(path.join(__dirname + '/pages/main.html'));
+});
+
+app.get('/pages/forecast.html', function(req,res){
+	console.log('forecast requested');
+	res.sendFile(path.join(__dirname + '/pages/forecast.html'));
+});
+
 
 app.listen(PORT, function(){
 	console.log("Listening on port: " + PORT);
