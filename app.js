a71f5a588218c87bbc477597e5d87d3b465821f0
Diego Prats <diego.prats@gmail.com>
2015-07-16 20:05:59 -0700

    lecture 44 additions

--- app.js
+++ app.js
@@ -15,21 +15,67 @@ myApp.config(function($routeProvider){
 		templateUrl: 'pages/forecast.html',
 		controller: 'forecastController'
 	})
+	.when('/forecast/:days', {
+		templateUrl: 'pages/forecast.html',
+		controller: 'forecastController'
+	})
 
 
 });
 
+//SERVICES
+
+myApp.service('cityService', function(){
+
+	this.city = 'Boston';
+
+});
+
+
+
 
 // CONTROLLERS
 
-weatherApp.controller('mainController', ['$scope', function($scope){
+myApp.controller('mainController', ['$scope','$resource','cityService', function($scope,$resource, cityService){
 
-	console.log('Hello from mainController');
+	$scope.city = cityService.city;
+	$scope.$watch('city', function(){
+		cityService.city = $scope.city;
+	});
+
+
 
 }]);
 
-weatherApp.controller('forecastController', ['$scope', function($scope){
+myApp.controller('forecastController', ['$scope','$resource','$routeParams', 'cityService', function($scope,$resource, $routeParams, cityService){
 
-	console.log('Hello from forecastController');
+
+	$scope.city = cityService.city;
+
+	// console.log('cityService.city in forecast is: ' + $scope.city);
+
+	$scope.days = $routeParams.days || 2;
+
+		//ng-resource weaps up the $http service in angularJS
+	$scope.weatherApi = $resource("http://api.openweathermap.org/data/2.5/forecast/daily", {
+		callback: "JSON_CALLBACK" }, { get: { method: "JSONP" } });
+
+	$scope.weatherResults = $scope.weatherApi.get({
+		q: $scope.city, cnt: $scope.days
+	});
+
+	console.log("days: " + $scope.days);
+
+	$scope.convertToFahrenheit = function(degK){
+		return Math.round((1.8 * (degK - 273)) + 32);
+	}
+
+	$scope.convertToDate = function(dt){
+		return new Date(dt * 1000);//because API returns answer in milliseconds
+
+	}
+
 
 }]);
+
+
